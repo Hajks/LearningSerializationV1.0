175e3c39e7a02293b115edc6e6a1246c004825da
Hajk <tunczorx@gmail.com>
2018-07-10 08:14:20 +0200

    Adding missing files.

---
+++ LearningSerializationV1.0/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSerializationV1._0
+{
+    [Serializable]
+    class Box
+    {
+        public string Name;
+        public Box(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
--- LearningSerializationV1.0/Form1.cs
+++ LearningSerializationV1.0/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace LearningSerializationV1._0
 {
@@ -15,6 +17,17 @@ namespace LearningSerializationV1._0
         public Form1()
         {
             InitializeComponent();
+            BinaryFormatter formatter = new BinaryFormatter();
+            Box box = new Box("small box");
+            string folderPath = @"C: \Users\Praca\Desktop\Visual StreamWriter\testingObjects.dat";
+            using (Stream output = File.Create(folderPath))
+            {
+                formatter.Serialize(output, box);
+            }
+            using (Stream input = File.OpenRead(folderPath)) //It works, if we delete stream output stream input can still load data from saved file !
+            {
+                Box box2 = (Box)formatter.Deserialize(input);
+            }
         }
     }
 }
